5a9c76bd0ea6833b212da21a6abb0bf165a1b90f
kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-04 15:20:32 +0300

    Created the API end point that fetches specific order

--- firstfood.py
+++ firstfood.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, request #import objects from the Flask model
 
 app = Flask(__name__, template_folder='v1') #define app and telling flask that template folder is named v1
-orders = [{'name':'cofee'}, {'name':'Beaf'}]
+orders = [{'name':'coffee'}, {'name':'Beaf'}]
 
 @app.route('/v1')
 def welcome():
@@ -11,9 +11,18 @@ def welcome():
 def getOrders():
     return jsonify({'message' : 'Itworks!'})
 
-@app.route('/v1/all_orders', methods=['GET'])
+@app.route('/v1/all_orders', methods=['GET']) # GET API that gets all orders
 def returnAll():
     return jsonify({'orders': orders})
+
+@app.route('/v1/all_orders/<string:name>', methods=['GET']) # fetch Specific order
+def returnOne(name):
+    ords=[order for order in orders if order['name']== name]
+    return jsonify({'order' : ords[0]})
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
