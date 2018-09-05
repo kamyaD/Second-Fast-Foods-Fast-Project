707a9a23841ec33f17504b6041c16c7218093f1b
kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-05 16:55:30 +0300

    add test file

---
+++ firstfood_test.py
@@ -0,0 +1,12 @@
+import unittest
+from  firstfood import returnAll
+
+class test_odres_returnAll(unittest.TestCase):
+    def test_return_all_orders(self):
+        self.assertEqual(returnAll("johndoe", "johndoe")"johndoe", "johndoe")
+     
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
