5e1055505b916d273d9f91148fedef495628a191
kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-05 17:54:23 +0300

    remove dummy test on the test file

--- firstfood_test.py
+++ firstfood_test.py
@@ -3,8 +3,7 @@ from  firstfood import returnAll
 
 class test_odres_returnAll(unittest.TestCase):
     def test_return_all_orders(self):
-        self.assertEqual(returnAll("johndoe", "johndoe")"johndoe", "johndoe")
-     
+        pass
     
 
 
