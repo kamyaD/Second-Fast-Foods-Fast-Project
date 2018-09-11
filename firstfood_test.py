887c77cde877b0e031e6c9e083ea98853fe689dc
kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-11 09:55:02 +0300

    create test for POST api

--- firstfood_test.py
+++ firstfood_test.py
@@ -1,6 +1,7 @@
 from firstfood import app 
 from firstfood import orders
 import unittest
+import json
 
 class WelcomeTestCase(unittest.TestCase):
     
@@ -23,6 +24,17 @@ class WelcomeTestCase(unittest.TestCase):
             '/api/v1/all_orders', content_type='html/text')
         self.assertTrue(b'coffee' in response.data)
 
+    def test_addOrder(self):
+        tester = app.test_client(self)
+        self.orders = [{'name':'coffee'}, {'name':'Beaf'},{'name' : 'Milk'}]
+        self.order= {'name':'Bread'}
+        self.new_orders=orders.append(self.order)
+        
+        response = tester.post(
+            '/api/v1/all_orders', data= self.new_orders)
+        self.assertTrue(response.status_code, 201 )
 
+        
 if __name__ == '__main__':
     unittest.main()
+
