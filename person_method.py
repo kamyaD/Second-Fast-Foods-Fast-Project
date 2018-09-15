37cf3a88c4e1d424c465513435f6995edd50a315
kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-15 13:58:22 +0300

    moved venv files to .ignore file and deleted the folder

---
+++ person_method.py
@@ -0,0 +1,36 @@
+class Person():
+    """docstring for Person"""
+    def__init__(self, name, age, gender)
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+def firstName(self):
+    fname = self.name.split[0]
+    return fname
+
+def lastName(self):
+    lname = self.name.split()[1]
+    return lname
+
+def body_mass_index(self,height):
+    bmi = self.mass / (height*height)
+    return bmi
+
+def gender (self):
+    gen=self.gender
+    return gen
+
+class Employee(Person):
+    def _init_(self,salary,jd,dpt):
+        self.name = Person.firstName()
+        self.salary = salary
+        self.jd = jd
+        self.dpt = dpt
+
+    def return_one_employee():
+        return "{} is a {} and earns a salary of {}".format(self.jd, self.salary)
+
+
+
+
