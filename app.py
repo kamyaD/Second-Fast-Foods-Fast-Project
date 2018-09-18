1b675bd9a12ae675e96f09a80b2207b51b7827b4
kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-18 20:43:00 +0300

    renamed app file

--- app.py
+++ app.py
@@ -3,28 +3,10 @@
 from flask import Flask, render_template, jsonify, request,session,flash,redirect,url_for 
 
 app = Flask(__name__, template_folder='v1') #define app and telling flask that template folder is named v1
-orders = [{'name':'coffee'}, {'name':'Beaf'},{'name' : 'Milk'}] # Making a Dictionary of orders that is to be used to test the code
-app.secret_key= "I love kenya"
-
-@app.route('/v1')
-def welcome():
-    return render_template("welcome.html") 
-
-@app.route('/v1/histry')
-def histry():
-    return render_template("histry.html")
-
-
-@app.route('/v1/order')
-def order():
-    return render_template("order.html")
-
-@app.route('/v1/add_order')
-def add_order():
-    return render_template("add_order.html")
+orders = [{'name':'coffee'}, {'name':'Beef'},{'name' : 'Milk'}] # Making a Dictionary of orders that is to be used to test the code
 
 @app.route('/api/v1/order', methods=['GET']) #Testing the jsonify out put on a browser
-def getOrders():
+def output():
     return jsonify({'message' : 'Itworks!'})
 
 @app.route('/api/v1/all_orders', methods=['GET']) # GET API that gets all orders
@@ -53,24 +35,6 @@ def deleteOrder(name):
     delOrder=[order for order in orders if order['name']== name]
     orders.remove(delOrder[0])
     return jsonify({'orders': orders})
-
-@app.route('/v1/login', methods=['GET','POST']) # Login APIs
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'domnic' or request.form['password'] != 'domnic':
-            error='Invalid credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            #flash('You are now logged in!')
-            return redirect(url_for('order'))
-    return render_template('login.html', error=error)
-
-@app.route('/v1/logout')
-def logout():
-    session.pop('logged_in',None)
-    flash('You are now  loged out!')
-    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=False)
